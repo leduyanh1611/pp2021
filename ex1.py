8ec412dfb71f44606c5b2b7237ea03a9c1df2434
redhorizon0309 <redhorizon0309@gmail.com>
2021-04-26 15:41:51 +0700

    [UPLOAD]Labwork1

---
+++ ex1.py
@@ -0,0 +1,14 @@
+x = 3
+print (" Input Number of students in a class: ", x)
+list1 = ('BI9-034', 'LeDuyAnh', '16/11/2000')
+list2 = ('123-546', 'abcdef', '11/22/3333')
+list3 = ('678-987', 'ghijkl', '44/55/6666')
+print(" Student information: id, name, DoB: ", list1, list2,list3)
+y = 10
+print(" Number of courses: ", y)
+tup1 = ('PY', 'Python')
+tup2 = ('OOP', 'Object oriented programming')
+
+print(" Course information: id, name : ", tup1, tup2)
+z = 100
+print(" Marks for student in this course: ", z)
