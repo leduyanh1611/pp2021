e390bdbac2e96a890a4e87d8723ae9272c9cb780
leduyanh1611 <leduyanh1611@gmail.com>
2021-06-04 01:37:40 +0700

    labwork5

---
+++ labwork5/domain/course.py
@@ -0,0 +1,15 @@
+course=[]
+idcourse=[]
+cre=[]
+class cFourses:
+    def __init__(self,id,name,cre_course):
+        self.id=id
+        self.name=name
+        self.cre_course=cre_course
+    def get_id(self):
+        return self.id
+    def get_name(self):
+        return self.name 
+    def get_cre_course(self):
+        return self.cre_course
+
